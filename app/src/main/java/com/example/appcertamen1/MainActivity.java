2806211c48b2cf38edc8d3e05ab169249b972c5a
Work <udecamtres@gmail.com>
2024-11-10 02:14:56 -0300

    Definicion de variables de texto, listView, lista y adaptador en el sistema. Se procedio con cada busqueda de sus ID's. Finalmente se está en proceso de implementar el boton de agregar.

--- app/src/main/java/com/example/appcertamen1/MainActivity.java
+++ app/src/main/java/com/example/appcertamen1/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.appcertamen1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtIngresoItem1, txtIngresoItem2, txtIngresoItem3;
+    ListView lista;
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtIngresoItem1 = findViewById(R.id.txtIngreso1);
+        txtIngresoItem2 = findViewById(R.id.txtIngreso2);
+        txtIngresoItem3 = findViewById(R.id.txtIngreso3);
+        //ListView
+        lista = findViewById(R.id.listVieww);
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,items);
+        //Setteamos el adapter
+        lista.setAdapter(adapter);
+    }
+
+    public void agregarItem (View view)
+    {
+
     }
 }
